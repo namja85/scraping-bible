41dba4211a88a53773c051f69f1f06883b4aec6f
GyeongeunHyun <gehyun@osstem.com>
2022-05-09 17:52:49 +0900

    성경 순서 오류 수정

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const utils = require('./utils');
 
 async function createBibleObject() {
   const bibleList = await utils.getBibleList();
+  const titles = await utils.getBibleTitleEnglish();
 
   for (const bible of bibleList) {
     const cnMax = await utils.getMaxCnEachBible(bible.vlIndex);
     bible['cnMax'] = cnMax;
+    bible.title['english'] = titles[bible.title.korean];
   }
 
   console.log(bibleList);
@@ -19,13 +21,24 @@ async function createBibleObject() {
 async function createMarkdownfile(bible) {
   for (const { title, vlIndex, cnMax } of bible) {
     for (let i = 1; i <= cnMax; i++) {
-      const content = await utils.getContentEachBible(vlIndex, i);
-      const fileContent = `---\ntitle: ${title.english}\n---\n\n#${title.korean} (${title.english}) ${i}장\n${content.map((item) => '- ' + item).join('\n')}`;
+      try {
+        const dirPath = path.join(__dirname, '../output');
+        const filePath = path.join(dirPath, `${title.english}-${i}.md`);
 
-      const dirPath = path.join(__dirname, '../output');
-      const filePath = path.join(dirPath, `${title.english}-${i}.md`);
-      console.log(filePath);
-      fs.writeFileSync(filePath, fileContent);
+        if (!fs.existsSync(filePath)) {
+          const content = await utils.getContentEachBible(vlIndex, i);
+          const fileContent = `---\ntitle: ${title.english}\n---\n\n# ${title.korean} (${title.english}) ${i}장\n${content.map((item) => '- ' + item).join('\n')}`;
+          
+          fs.writeFileSync(filePath, fileContent);
+          await utils.sleepMs(300);
+
+          console.log(filePath, 'created');
+        } else {
+          console.log(filePath, 'exist');
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 }
--- src/utils.js
+++ src/utils.js
@@ -12,60 +12,97 @@ async function getHtml(url, decode = 'UTF-8') {
     });
     return iconv.decode(res.data, decode).toString();
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 }
 
 async function getBibleList() {
-  const bibleList = [];
-  const content = await getHtml(bible_list_url(), 'EUC-KR');
-  const $ = cheerio.load(content);
-  const $list = $('.tk3').find('a[href^="B_GAE/cgi/bibleftxt.php"]');
+  try {
+    const bibleList = [];
+    const content = await getHtml(bible_list_url(), 'EUC-KR');
+    const $ = cheerio.load(content);
+    const $list = $('select[name="VL"]').find('option');
+    
+    $list.each((_, elem) => {
+      const title = $(elem).text().trim();
+      const vlIndex = parseInt($(elem).attr('value'));
+      
+      bibleList.push({ title: { korean: title }, vlIndex });
+    });
+    
+    return bibleList;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-  $list.each((index, elem) => {
-    const title = $(elem).text().trim();
-    const [all, korean, english] = title.match(/([가-힣0-9]+)\s\((.*)\)/);
-    const vlIndex = index + 1;
-
-    bibleList.push({ title: { korean, english }, vlIndex });
-  });
-
-  return bibleList;
+async function getBibleTitleEnglish() {
+  try {
+    const titles = {};
+    const content = await getHtml(bible_list_url(), 'EUC-KR');
+    const $ = cheerio.load(content);
+    const $list = $('.tk3').find('a[href^="B_GAE/cgi/bibleftxt.php"]');
+    
+    $list.each((_, elem) => {
+      const title = $(elem).text().trim();
+      const [all, korean, english] = title.match(/([가-힣0-9 ]+)\s\((.*)\)/);
+      
+      titles[korean] = english;
+    });
+    
+    return titles;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getMaxCnEachBible(vlIndex) {
-  const content = await getHtml(bible_url(vlIndex, 1), 'EUC-KR');
-  const $ = cheerio.load(content);
-  const $list = $('.tk3').find('a[href^="B_GAE/cgi/bibleftxt.php"]');
-  const cnList = [];
-
-  $list.each((_, elem) => {
-    const number = parseInt($(elem).text());
-    if (!Number.isNaN(number)) {
-      cnList.push(number);
-    }
-  });
-
-  return cnList.length ? cnList[cnList.length - 1] : 1;
+  try {
+    const content = await getHtml(bible_url(vlIndex, 1), 'EUC-KR');
+    const $ = cheerio.load(content);
+    const $list = $('.tk3').find('a[href^="B_GAE/cgi/bibleftxt.php"]');
+    const cnList = [];
+    
+    $list.each((_, elem) => {
+      const number = parseInt($(elem).text());
+      if (!Number.isNaN(number)) {
+        cnList.push(number);
+      }
+    });
+    
+    return cnList.length ? cnList[cnList.length - 1] : 1;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getContentEachBible(vlIndex, cnIndex) {
-  const content = await getHtml(bible_url(vlIndex, cnIndex), 'EUC-KR');
-  const $ = cheerio.load(content);
-  const $list = $('.tk4br').find('li');
-  const contentList = [];
+  try {
+    const content = await getHtml(bible_url(vlIndex, cnIndex), 'EUC-KR');
+    const $ = cheerio.load(content);
+    const $list = $('.tk4br').find('li');
+    const contentList = [];
+    
+    $list.each((_, elem) => {
+      contentList.push($(elem).text().trim());
+    });
+    
+    return contentList;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-  $list.each((_, elem) => {
-    contentList.push($(elem).text().trim());
-  });
-
-  return contentList;
+function sleepMs(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 module.exports = {
   bible_list_url,
   bible_url,
   getBibleList,
+  getBibleTitleEnglish,
   getMaxCnEachBible,
-  getContentEachBible
+  getContentEachBible,
+  sleepMs
 };
